eceb512c9941c9a6f2067923ab036c9429649ccb
AriDela <55457916+Nitr0gue@users.noreply.github.com>
2023-01-26 10:01:37 -0600

    triangulation testing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/testAuto.java
@@ -0,0 +1,143 @@
+package org.firstinspires.ftc.teamcode.Auto;
+
+import org.firstinspires.ftc.teamcode.Auto.triangulationTest;
+import org.firstinspires.ftc.teamcode.Auto.fullTriangulation;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.opencv.calib3d.Calib3d;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Point;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.imgproc.Moments;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+
+@Autonomous (name="testAuto")
+
+public class testAuto extends LinearOpMode {
+
+    // position variables
+    private int liftPos;
+    private double bucketPos;
+    private double distanceToHub;
+    private double bucketDumpPos;
+
+    //define dcmotors
+    DcMotor leftFront = null;
+    DcMotor rightFront = null;
+    DcMotor leftBack = null;
+    DcMotor rightBack = null;
+    DcMotor arm = null;
+    DcMotor liftMotor1 = null;
+    DcMotor liftMotor2 = null;
+
+    public Servo claw;
+
+    //drivetrain variables (would have been used in drive method at the bottom)
+    int leftPos;
+    int rightPos;
+
+    //arm variable
+    int armPos;
+
+
+    @Override
+    public void runOpMode() {
+        //get motors from the hardware map (in the quotations are what the hardware objects are called in the configurations part of the driver station)
+
+
+        final int width = 320;
+        final int height = 240;
+        boolean test = false;
+
+
+        // This has to do with the computer vision. The other file in this package called SignalReader is the camera configuration portion
+        SignalReader detector = new SignalReader(width);
+        OpenCvCamera camera1;
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        camera1 = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "webcam1"), cameraMonitorViewId);
+        camera1.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                camera1.startStreaming(width, height, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+
+            }
+        });
+        camera1.setPipeline(detector);
+
+        OpenCvCamera camera2;
+        int cameraMonitorViewId1 = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId1", "id", hardwareMap.appContext.getPackageName());
+        camera2 = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "webcam2"), cameraMonitorViewId1);
+        camera2.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                camera2.startStreaming(width, height, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+
+            }
+        });
+        camera2.setPipeline(detector);
+
+
+
+
+
+
+        //camera.startStreaming(width, height, OpenCvCameraRotation.UPRIGHT);
+        // These statements below show up on the driver station and detect the average amounts of RGB in the frame created in SignalReader
+
+        while (!isStarted()) {
+            telemetry.addData("Position from camera 1: (", + x1 + ", ", + y1 + ")");
+            telemetry.addData("Position from camera 2: (", + x2 + ", ", + y2 + ")");
+            telemetry.addData("3D Position of the object: ", objectPoints.toString());
+
+
+            telemetry.update();
+        }
+
+
+        //these set the position variables to 0 at the beginning
+        int leftPos = 0;
+        int rightPos = 0;
+
+        int armPos = 0;
+
+        int liftPos = 0;
+
+        //closes the claw on the preloaded cone before the autonomous is initialized
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+
+        //RED
+
+
+
+    }
+
+
+    //private void drive(int leftTarget, int rightTarget, double speed, int armTarget, int liftTarget){
+    // this is a custom method that updates the speed, the arm value, and the lift values during the autonomous (see above)
+
+}
+
